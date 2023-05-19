a0e856ddfa47a77f817d2f81ef47a85d74e119e0
Wargamer-Senpai <gamertrend1@gmail.com>
2023-05-19 17:29:30 +0200

    added logs, added command toggle, added update msg

--- config.py
+++ config.py
@@ -52,6 +52,13 @@ stats_visible = "public"
 # only works if gihpy api is not empty
 bad_word_checks = False
 
+# set message when the bot detects a new version 
+# you can use <@_|everyone> to ping everyone
+# the new found version will always be added later on, when sending the message, it will look like this:
+# @everyone it looks like a new update arrived, Version: beta74
+matrix_update_message = "<@_|everyone> it looks like a new update arrived, Version: " 
+
+
 
 # command prefix 
 command_prefix = "!"
@@ -81,30 +88,44 @@ command_admin_leave = command_base_admin + "leave-room"
 command_admin_stats = command_base_admin + "stats"
 command_admin_reload = command_base_admin + "reload"
 command_admin_autojoin = command_base_admin + "autojoin"
+command_admin_notify = command_base_admin + "update-notify"
 
-# normal user help page 
-help_display = {
-    command_gif: "send random gif or use search tag `!gif <search>`",
-    command_stats: "display gathered stats",
-    command_help: "display this list",
-    command_ping: "lets ping pong",
-    command_btc: "display prices of Bitcoin",
-    command_eth: "display prices of Ethereum",
-    command_whoami: "tells you your identifier and name and if your admin",
-    command_whois: "parse the matrix identifier and i will find the name of the person `"+ command_prefix + command_whois+" <identifier>`",
-    command_dice: "roll a number between 1 and 6",
-    command_poll: "start a poll with emojis to vote, example `!poll should we do polls?` more info [here](https://github.com/Wargamer-Senpai/teampy/wiki/Overview#poll-string)"
+# normal user command overview
+# toggle command with True or False
+commands_overview = {
+    command_gif: {"description": "send random gif or use search tag `!gif <search>`", "command_enabled": True},
+    command_stats: {"description": "display gathered stats", "command_enabled": True},
+    command_help: {"description": "display this list", "command_enabled": True},
+    command_ping: {"description": "lets ping pong", "command_enabled": True},
+    command_btc: {"description": "display prices of Bitcoin", "command_enabled": True},
+    command_eth: {"description": "display prices of Ethereum", "command_enabled": True},
+    command_whoami: {"description": "tells you your identifier and name and if your admin", "command_enabled": True},
+    command_whois: {"description": "parse the matrix identifier and i will find the name of the person `"+ command_prefix + command_whois+" <identifier>`", "command_enabled": True},
+    command_dice: {"description": "roll a number between 1 and 6", "command_enabled": True},
+    command_poll: {"description": "start a poll with emojis to vote, example `!poll should we do polls?` \
+                    more info [here](https://github.com/Wargamer-Senpai/teampy/wiki/Overview#poll-string)", "command_enabled": True}
     }
 
-# admin help page 
-admin_help_display = {
-    command_admin_help: "display this admin help",
-    command_admin_version: "display version of bot, and checks for new version",
-    command_admin_health: "checks health of bot, if there is a problem it hopefully will display it",
-    command_admin_stop: "stops the bot",
-    command_admin_restart: "restarts the bot",
-    command_admin_leave: "tells the bot to leave the current room",
-    command_admin_stats: "display gathered stats",
-    command_admin_reload: "reload current config",
-    command_admin_autojoin: "toggle auto join for rooms and direct chats on invite to on or off"
+# admin command overview 
+# toggle command with True or False
+admin_commands_overview = {
+    command_admin_help: {"description": "display this admin help", "command_enabled": True},
+    command_admin_version: {"description": "display version of bot, and checks for new version", "command_enabled": True},
+    command_admin_health: {"description": "checks health of bot, if there is a problem it hopefully will display it", "command_enabled": True},
+    command_admin_stop: {"description": "stops the bot", "command_enabled": True},
+    command_admin_restart: {"description": "restarts the bot", "command_enabled": True},
+    command_admin_leave: {"description": "tells the bot to leave the current room", "command_enabled": True},
+    command_admin_stats: {"description": "display gathered stats", "command_enabled": True},
+    command_admin_reload: {"description": "reload current config", "command_enabled": True},
+    command_admin_autojoin: {"description": "toggle auto join for rooms and direct chats on invite to on or off", "command_enabled": True},
+    command_admin_notify: {"description": "toggle notifcation, for room the command was entered in", "command_enabled": True}
     }   
+
+
+
+#
+# set log level 
+#
+# Possible values: CRITICAL, ERROR, WARNING, INFO, DEBUG
+# default and recommended: "INFO" 
+log_level="INFO"
