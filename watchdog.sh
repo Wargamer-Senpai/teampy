b3b91a45b161518597188cb38887f32106ffd1e1
Wargamer-Senpai <gamertrend@t-online.de>
2023-12-06 17:26:39 +0100

    reworked restart behavior

--- watchdog.sh
+++ watchdog.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
-if [[ $CONTAINER_BOOL == "True" ]]; then
-  COUNT_RAM_FAILS=0
-  KILL_CONTAINER=false
+#
+#
+# this is a script to watch over the bot, the bot for some reason crashes/freezes inside a container.
+# 
+BOT_PID=0
+
+
+# function kill_container(){
+#   exit 69
+# }
+
+function start_bot(){
+  if [[ $BOT_PID -ne 0 ]]; then
+    kill -9 $BOT_PID
+  fi
 
   /usr/bin/python3 /opt/teampy/main.py &
-  STARTUP_CODE=$?
-  if [[ $STARTUP_CODE -ne 0 ]]; then
-    echo "[ERROR] Startup failed (Code $STARTUP_CODE)"
-    exit 5
-  fi
+  BOT_PID=$!
+  /bin/sleep 60
+}
+
+
+if [[ $CONTAINER_BOOL == "True" ]]; then
+  COUNT_RAM_FAILS=0
+  echo "Started Container $(date)"
+  start_bot
 
   # CHECK RAM
   while true; do
+    # check change timestamp
+    TIMESTAMP_FILE=$(stat check_container | grep Change | awk -F: '{print $4}' | awk -F. '{print $1}')
+    TIMESTAMP_DATE=$(date | awk -F: '{print $3}' | awk '{print $1}')
+    TIMESTAMP_DIFF=$((10#$TIMESTAMP_DATE-10#$TIMESTAMP_FILE))
+
+    # if time difrence is greater then 10 seconds kill, #0 casts the number to be an INT not Octal
+    if [[ ${TIMESTAMP_DIFF#0} -gt 10 ]]; then
+      echo '[ERROR] Bot didnt touched file, killing the bot *bonk*'
+      start_bot
+    fi
+
     # get ram usage
     # shellcheck disable=SC2062
     RAM_USAGE=$(top -n 1 | grep python[3] | awk '{print $8}') 
@@ -22,16 +49,14 @@ if [[ $CONTAINER_BOOL == "True" ]]; then
     else
       COUNT_RAM_FAILS=0
     fi 
+
     if [[ $COUNT_RAM_FAILS -eq 50 ]]; then
-      KILL_CONTAINER=true
+      echo '[ERROR] Bot didnt used ram for a long time, killing the bot *bonk*'
+      start_bot
     fi
     if [[ "$RAM_USAGE" == "" || "$RAM_USAGE" == " " || -z "$RAM_USAGE" ]]; then
-      KILL_CONTAINER=true
-    fi
-
-    # kill the bot if it gues its to much
-    if $KILL_CONTAINER; then
-      exit 69
+      echo '[ERROR] Where is the PID, killing the bot *bonk*'
+      start_bot
     fi
 
     /bin/sleep 1
