aad6ff35a4e95f8ee1c82481fbc9d5d47fd13dda
Wargamer-Senpai <gamertrend1@gmail.com>
2025-05-03 14:44:26 +0200

    update docstring for func_write_to_log to clarify parameters and usage

--- modules/logger.py
+++ modules/logger.py
@@ -4,6 +4,14 @@ import os
 import config
 
 def func_write_to_log(log_message, log_level, log_function, log_file=os.path.join(os.path.abspath(__file__),"..","..","logs","bot.log")):
+  """writes log messages to a file and prints them to the console if debug mode is enabled
+
+  Args:
+      log_message (str): message to be logged
+      log_level (str): log level of the message, possible values: CRITICAL, ERROR, WARNING, INFO, DEBUG
+      log_function (str): name of the function that is logging the message
+      log_file (str, optional): the log file, defaults to os.path.join(os.path.abspath(__file__),"..","..","logs","bot.log").
+  """
   # fallback if conf_log_level has been wrongly configured
   config_log_level = config.log_level
   if not config_log_level in ["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"]:
